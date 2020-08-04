c5ba745a27f23a6e045c3ae1602fe16602b6a85d
Chris <oaoxd0314@gmail.com>
2020-08-04 15:49:29 +0800

    netlify fix

--- src/componment/RoomsContainer.js
+++ src/componment/RoomsContainer.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import RoomsFilter from './RoomsFilter';
-import RoomsList from './RoomsList';
-
+import React from "react";
+import RoomsFilter from "./RoomsFilter";
+import RoomsList from "./RoomsList";
+import { RoomConsumer } from "../context";
+import Loading from "./Loading";
 export default function RoomsContainer() {
-    return (
-        <div>
-            <RoomsFilter/>
+  return (
+    <RoomConsumer>
+      {(value) => {
+          console.log(value)
+        return (
+          <div>
+            <RoomsFilter />
             <RoomsList />
-        </div>
-    )
+          </div>
+        );
+      }}
+    </RoomConsumer>
+  );
 }
